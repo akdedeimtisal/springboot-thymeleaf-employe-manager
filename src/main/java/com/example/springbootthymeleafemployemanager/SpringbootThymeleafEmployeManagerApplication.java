c1b833370bff66aaafc9275b93ad6568d1b65448
akdedeimtisal <akdedeimtisall@gmail.com>
2022-02-28 18:03:54 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootthymeleafemployemanager/SpringbootThymeleafEmployeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootthymeleafemployemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafEmployeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafEmployeManagerApplication.class, args);
+	}
+
+}
